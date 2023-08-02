1a3ddf9f764bee20d72d2a5c134f65f41e1d9eea
TFink7 <98671947+TFink7@users.noreply.github.com>
2023-08-02 03:06:23 -0400

    Add weather service and model

---
+++ WeatherApp/Models/Weather.cs
@@ -0,0 +1,28 @@
+//Weather class encapsulates weather data for specific locations to simplify data management.
+using Newtonsoft.Json;
+namespace WeatherApp.Models
+{
+    public class Weather
+    {
+        public string Location { get; set; }
+        public double LocalTemperature { get; set; }
+        public double FeelsLike { get; set; }
+        public double Humidity { get; set; }
+        public double ChanceOfRain { get; set; }
+        public double High { get; set; }
+        public double Low { get; set; }
+        public string WeatherConditions { get; set; }
+
+        public Weather(string location, double localTemperature, double feelsLike, double humidity, double chanceOfRain, double high, double low, string weatherConditions)
+        {
+            Location = location;
+            LocalTemperature = localTemperature;
+            FeelsLike = feelsLike;
+            Humidity = humidity;
+            ChanceOfRain = chanceOfRain;
+            High = high;
+            Low = low;
+            WeatherConditions = weatherConditions;
+        }
+    }
+}
